d812a573d8d7ccb86403e6182b9397271301378a
NathanMuniz <natangabriel.palmeiras@gmail.com>
2022-07-31 15:14:00 -0300

    Set git in the linux, finished Twitch service

--- src/app/Service/TwitchService.php
+++ src/app/Service/TwitchService.php
@@ -1,78 +1,47 @@
-<?php 
+<? php
 
-namespace App\Services;
+namespace App\Service; 
 
-use Guzzle\Client;
+use GuzzleHttp\Client;
 
 
-class TwitchService
+
+class TwithService
 {
 
-    private $client;
+  private $client;
 
-    public function __construct()
-    {
-        $client = new Client([
-            "base_uri" => 'https://api.twitch.tv/helix/',
-            "timeout" => 0.5
-        ]);
+  public function __construcutor()
+  {
+      
+    $client = new Client([
+      'base_uri' => 'https://api.twitch.tv/helix/',
+      'timeout' => 0.5      
+    ])
+  }
 
-    }
+  public function auth(string $code): array 
+  {
 
-    public function auth(string $code) : array
-    {
-        $uri = 'https://id.twitch.tv/oauth2/toke';
-        
-        $response = $this->client->request('POST', $uri, [
-            "form_param" => [
-                "client_id" => config("connectors.twitch.client_Id"),
-                "client_secret" => config("connectors.twitch.client_service"),
-                "grant_type" => "authorization_code",
-                "code" => $code,
-                "redrict_uri" => config("connectos.twitchs.client_redirect")
-                ]
+    $uri = 'https://id.twitch.tv/oauth2/toke';
+    
+    $response = $this->client->request('POST', $uri, [
+      "form_param" => [
+        "client_id" => config('connectors.twitch.client_id'),
+        "client_secret" => config('connectors.twitch.client_secret'),
+        "grant_type" => 'authorization_code',
+        "code" => $code,
+        "redirice_uri" => config('connectors.twitch.redirice_uri')
+      ]
+    ])
 
-            ]);
-        
-        return json_decode($response->getBody(), true);
 
-    }
 
-    public function getAuthenticatedUser(string $token): array
-    {
-        $uri = "users";
-        $response = $this->client->request('GET', $uri, [
-            'headers' => [
-                'Client-ID' => config('connectors.twitch.client_id'),
-                'Authorization' => 'Bearer ' . $token
-            ]
-        ]);
-
-        $result = json_decode($response->getBody(), true)['data'][0];
-        return [
-            'id' => $result['id'],
-            'username' => $result['login'],
-            'email' => $result['email']
-        ];
-    }
-
-    public function revokeToken(string $token): bool
-    {
-        $uri = 'https://id.twitch.tv/oauth2/revoke';
-        $response = $this->client->request('POST', $uri, [
-            'query' => [
-                'client_id' => config('connectors.twitch.client_id'),
-                'client_secret' => config('connectors.twitch.client_secret'),
-                'token' => $token
-            ]
-        ]);
-        dump($response);
-
-        return true;
-    }
+  }
 
 
 }
+
 
 
 
