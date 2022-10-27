18cbeca4e00648e333d7e480eb44e4706c9f8a8f
Qubitsys <qubitsysx@gmail.com>
2022-10-27 18:08:33 -0300

    remake all the application

--- src/resources/views/template/navbar.blade.php
+++
@@ -1,5 +0,0 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <div class="container">
-    <a class="navbar-brand" href=" {{ config('app.name') }} "></a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse"></button>
-
