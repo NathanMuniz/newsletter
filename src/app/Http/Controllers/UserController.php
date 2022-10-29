0bdb15ab58815112d573a65081d83b192e72781b
NathanMuniz <natangabriel.palmeiras@gmail.com>
2022-10-29 20:13:14 -0300

    Fazendo rotas e views

---
+++ src/app/Http/Controllers/UserController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+  public function welcome()
+  {
+    return view('welcome');
+  }
+  
+  public function login(req, res) 
+  {
+    
+  }
+
+
+
+};
